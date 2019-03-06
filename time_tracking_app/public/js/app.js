95d9fca7cdf2ec91b9e6845da46d889e4d6e79c2
Frida Erdal <fridaerdal@hotmail.no>
2019-03-06 09:31:36 +0100

    Time tracking app ongoing

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -83,50 +83,101 @@ class EditableTimer extends React.Component {
 }
 
 class ToggleableTimerForm extends React.Component {
-  tate = {
+  state = {
     isOpen: false,
   };
 
   handleFormOpen = () => {
-    this.setState({ isOpen: true });
+    this.setState({ isOpen: !this.state.isOpen});
   };
-  
+
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+  handleFormSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.setState({ isOpen: false });
+  };
+
+
   render() {
-    if (this.props.isOpen) {
+    if (this.state.isOpen) {
       return (
-            <TimerForm />
-          );
+        <TimerForm
+          onFormSubmit={this.handleFormSubmit}
+          onFormClose={this.handleFormClose}/>
+      );
     } else {
       return (
-          <div className='ui basic content center aligned segment'>
-            <button className='ui basic button icon'>
-              <i className='plus icon' />
-            </button>
-          </div> );
-        }
+        <div className='ui basic content center aligned segment'>
+          <button
+            className='ui basic button icon'
+            onClick={this.handleFormOpen}
+          >
+            <i className='plus icon' />
+          </button>
+        </div>
+      );
     }
+  }
 }
 
+
 class TimerForm extends React.Component {
+  state = {
+    title: this.props.title || '',
+    project: this.props.project || '',
+  };
+
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.value });
+  };
+
+  handleProjectChange = (e) => {
+    this.setState({ project: e.target.value });
+  };
+
+  handleSubmit = () => {
+    this.props.onFormSubmit({
+      id: this.props.id,
+      title: this.state.title,
+      project: this.state.project,
+    });
+  };
+
   render() {
-    const submitText = this.props.title ? 'Update' : 'Create';
+    const submitText = this.props.id ? 'Update' : 'Create';
     return (
       <div className='ui centered card'>
         <div className='content'>
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input
+                type='text'
+                value={this.state.title}
+                onChange={this.handleTitleChange}
+              />
             </div>
             <div className='field'>
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project} />
+              <input
+                type='text'
+                value={this.state.project}
+                onChange={this.handleProjectChange}
+              />
             </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
+              <button
+                className='ui basic blue button'
+                onClick={this.handleSubmit}
+              >
                 {submitText}
               </button>
-              <button className='ui basic red button'>
+              <button
+                className='ui basic red button'
+                onClick={this.props.onFormClose}
+              >
                 Cancel
               </button>
             </div>

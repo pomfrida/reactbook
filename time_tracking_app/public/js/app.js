dc3b5e36dbd37bd6754043f1987fd0cbf3d7ddd9
Frida Erdal <fridaerdal@hotmail.no>
2019-03-14 13:01:01 +0100

    continue page 151

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,22 +1,19 @@
 
 class TimersDashboard extends React.Component {
   state = {
-    timers: [
-      {
-        title: 'Practice squat',
-        project: 'Gym Chores',
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: 'Bake squash',
-        project: 'Kitchen Chores',
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
+    timers: [],
+  };
+
+  componentDidMount() {
+    this.loadTimersFromServer();
+    setInterval(this.loadTimersFromServer, 5000);
+  }
+
+  loadTimersFromServer = () => {
+    client.getTimers((serverTimers) => (
+        this.setState({ timers: serverTimers })
+      )
+    );
   };
 
   handleCreateFormSubmit = (timer) => {
@@ -44,6 +41,8 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.concat(t),
     });
+
+    client.createTimer(t);
   };
 
   updateTimer = (attrs) => {
@@ -59,12 +58,18 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.updateTimer(attrs);
   };
 
   deleteTimer = (timerId) => {
     this.setState({
       timers: this.state.timers.filter(t => t.id !== timerId)
     });
+
+    client.deleteTimer(
+      { id: timerId }
+    );
   }
 
   startTimer = (timerId) => {
@@ -81,6 +86,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.startTimer(
+      { id: timerId, start: now }
+    );
   };
 
   stopTimer = (timerId) => {
@@ -99,6 +108,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.stopTimer(
+      { id: timerId, stop: now }
+    );
   };
 
   render() {
@@ -356,9 +369,6 @@ class Timer extends React.Component {
               <i className='trash icon' />
             </span>
           </div>
-        </div>
-        <div className='ui bottom attached blue basic button'>
-          Start
         </div>
         <TimerActionButton
           timerIsRunning={!!this.props.runningSince}
